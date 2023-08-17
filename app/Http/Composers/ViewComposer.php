7a9c35dc0f653384f41d08c65aa0adc672ae1cdd
nikita <zvonovnikita42@gmail.com>
2023-08-17 16:15:53 +0700

    fix: changed approach of providing data to views

--- app/Http/Composers/ViewComposer.php
+++ app/Http/Composers/ViewComposer.php
@@ -2,17 +2,20 @@
 
 namespace App\Http\Composers;
 
-use App\Http\Controllers\PasteController;
-use App\Repositories\PasteRepository;
+use App\Services\PasteServiceInterface;
 use Illuminate\View\View;
 
 class ViewComposer
 {
-    public function compose(View $view) {
-        $pasteRepository = new PasteRepository();
+    private PasteServiceInterface $pasteService;
 
-        $publicPastes = $pasteRepository->lastPublicPastes();
-        $myPastes = $pasteRepository->lastPrivatePastes();
+    public function __construct(PasteServiceInterface $pasteService) {
+        $this->pasteService = $pasteService;
+    }
+
+    public function compose(View $view) {
+        $publicPastes = $this->pasteService->getLastPublic();
+        $myPastes = $this->pasteService->getLastPrivate();
 
         $view->with('publicPastes', $publicPastes)->with('myPastes', $myPastes);
     }
