d32e9668b34ce61401b11e81b0987e576827e21b
nikita <zvonovnikita42@gmail.com>
2023-08-17 15:09:20 +0700

    feat: interfaces for services were provided

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,10 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use App\Services\UserService;
+use App\Services\UserServiceInterface;
+use App\Services\PasteService;
+use App\Services\PasteServiceInterface;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -22,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+
+        $this->app->bind(UserServiceInterface::class, UserService::class);
+        $this->app->bind(PasteServiceInterface::class, PasteService::class);
     }
 }
